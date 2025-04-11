840b0eb08787c2cf5ef866b31d56dc874409e065
jagpreetrahi <jagpreet1615@gmail.com>
2025-04-11 13:26:49 +0530

    Hashing problem code'

---
+++ hashing/pattern_1.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+
+void NumberHashing(int arr[] , int size, int query[]){
+     
+    for(int i = 0; i<size; i++){
+       arr[i] == query[i]++
+    }
+
+}
+
+
+int main(){
+
+    int arr[] = {4,5,6,6,6,6,2};
+    
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int query[size + 1] = {0};
+     
+    NumberHashing(arr , size, query[]);
+   
+}
+
+
